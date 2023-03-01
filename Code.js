a1fcc53fd7fec0353cd8e08825ce09ee862359f8
Kaheel <112734438+Popskul56@users.noreply.github.com>
2023-02-28 20:32:37 -0700

    Add files via upload

--- Code.js
+++ Code.js
@@ -15,70 +15,22 @@ function runTimer( countdownElem){
      //this value will not change and creates the 5 seond delay
      var timeoutIncrement = 5000;
      
-     //begin of the countdown
-     setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
+    for(var counter = 0;counter < 10; counter++){
+        //begin of the countdown
+        setTimeout(function(){
+            if(currTime == 0){
+                alert("Takeoff!");
+                countdownElem.innerHTML = "Launch successful!";
+            }
+            else if (curTime <25){
+                countdownElem.innerHTML = "Warning Less than 1/2 way to launch, time left = " + currTime;
+            }
+            else{
+                countdownElem.innerHTML = currTime;
+            }
 
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + timeoutIncrement;
-
-    //This is last time we need to call setTimeout, currTime is now 0
-    setTimeout(function(){
-        alert("Takeoff!");
-        countdownElem.innerHTML = "Launch successful!";
-    }, timeout);
+            currTime = currTime - 5;
+        }, timeout);
+        timeout = timeout + timeoutIncrement;
+    }
 }
